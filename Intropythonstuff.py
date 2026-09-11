36f8ad8f3f849d4939715c0b86924c3a5b4bbdb8
Joshua Harvey <pleaseleaveamessageforjoshua@gmail.com>
2026-09-11 10:20:24 -0600

    Add files via upload

---
+++ Intropythonstuff.py
@@ -0,0 +1,32 @@
+print('intro python stuff')
+#comment - these do not execute
+# to comment multiple lines, highlight the lines and press ctrl +/
+
+
+#python is case sensitive. Spaces in code don't enter into the code. You can use spaces to make your code more readable.
+# Strings are a sequence of characters. You can use single or double quotes to create strings.
+#print it's a "groovy day" - this will not work because of the single quote in it's. you can use double quotes to fix this issue.
+print("it's a groovy day")
+
+#we can use escape sequences/characters to fix this and 
+
+print("It's a \"groovy\" day")
+print("This is a new line")
+#variables
+#a named container that holds a value
+#python variables are dynamically typed, meaning you don't have to declare the type of variable when you create it. The interpreter infers the type based on the value as signed to the variable. You can change the type of a variable by assigning a value of a different type to it.
+# names in python are snake_case.
+
+
+name = "Rob"
+age = 23
+bank_account = 123.76
+
+name = input("What is your name? ")
+print(name)
+name= "Rob"
+print(name)
+
+
+
+# f strings in the textbook. This is for convenience.
